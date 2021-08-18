4aa51e3feea79b5da14290e42a73173f5990957f
Davide Corsi <davide.corsi@par-tec.it>
2021-08-18 09:29:44 +0200

    first commit

---
+++ src/main/java/it/partec/ListStudents.java
@@ -0,0 +1,34 @@
+package it.partec;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ListStudents extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		JSONParser parser = new JSONParser();
+		JSONArray listStudent = null;
+		try(Reader file = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("liststudent.json"))) {
+			Object obj = parser.parse(file);
+			listStudent = (JSONArray) obj;
+			out.print(listStudent.toJSONString());
+		} catch(ParseException e) {
+			System.out.println(e);
+			response.setStatus(503);
+		}
+	}
+}
---
+++ src/main/java/it/partec/Student.java
@@ -0,0 +1,93 @@
+package it.partec;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Student extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		long id = Long.parseLong(request.getParameter("id")); // leggo il parametro dalla query string
+		JSONParser parser = new JSONParser();
+		JSONObject result = null; 
+		// leggo il file dalla cartella predisposta per contenere le risorse
+		try(Reader file = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("liststudent.json"))) {
+			Object obj = parser.parse(file);
+			JSONArray listStudent = (JSONArray) obj;
+			for(Object student: listStudent) {
+				JSONObject studentJson = (JSONObject) student;
+				if((Long) studentJson.get("id") == id) {
+					result = studentJson;
+					break;
+				}
+			}
+		} catch(ParseException e) {
+			System.out.println(e);
+		}
+		if(result != null) {
+			out.print(result.toJSONString());
+		} else {
+			// imposto l'http status 404
+			response.setStatus(404);
+			JSONObject error = new JSONObject();
+			error.put("error", "not found");
+			out.print(error.toJSONString());
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		JSONParser parser = new JSONParser();
+		// lettura dei parametri dal form
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		long age = Long.parseLong(request.getParameter("age"));
+		JSONArray listStudent = null;
+		JSONObject newStudent = new JSONObject();
+		long id = 0; 
+		try(Reader file = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("liststudent.json"))) {
+			Object obj = parser.parse(file);
+			listStudent = (JSONArray) obj;
+			for(Object student: listStudent) {
+				JSONObject studentJson = (JSONObject) student;
+				if((Long) studentJson.get("id") > id) {
+					id = (Long) studentJson.get("id");
+				}
+			}
+		} catch(ParseException e) {
+			System.out.println(e);
+		}
+		if(id != 0) {
+			newStudent.put("id", id + 1);
+			newStudent.put("name", name);
+			newStudent.put("surname", surname);
+			newStudent.put("age", age);
+			listStudent.add(newStudent);
+			// scrivo il file con il nuovo studente aggiunto
+			try(Writer file = new PrintWriter(getClass().getClassLoader().getResource("liststudent.json").getFile())) {
+				file.write(listStudent.toJSONString());
+			}
+			response.setStatus(201);
+		} else {
+			response.setStatus(503);
+		}
+
+	}
+
+
+}
